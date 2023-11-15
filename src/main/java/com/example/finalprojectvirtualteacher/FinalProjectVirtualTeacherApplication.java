94821fa592599f010b6348413f55f30380952d64
Dimitar <138873951+DimitarTsanev@users.noreply.github.com>
2023-11-15 19:31:50 +0200

    Add files via upload

---
+++ src/main/java/com/example/finalprojectvirtualteacher/FinalProjectVirtualTeacherApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectvirtualteacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectVirtualTeacherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectVirtualTeacherApplication.class, args);
+    }
+
+}
