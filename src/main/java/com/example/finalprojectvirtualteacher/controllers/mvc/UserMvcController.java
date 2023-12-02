fad7cb7171958f29dd81c57909745b8f86b5d1ca
KarinaIvanova <carrie.ivanova93@gmail.com>
2023-12-02 13:14:28 +0200

    Create Admin portal

--- src/main/java/com/example/finalprojectvirtualteacher/controllers/mvc/UserMvcController.java
+++ src/main/java/com/example/finalprojectvirtualteacher/controllers/mvc/UserMvcController.java
@@ -4,6 +4,7 @@ import com.example.finalprojectvirtualteacher.exceptions.AuthorizationException;
 import com.example.finalprojectvirtualteacher.helpers.AuthenticationHelper;
 import com.example.finalprojectvirtualteacher.helpers.ImageHelper;
 import com.example.finalprojectvirtualteacher.helpers.UserMapper;
+import com.example.finalprojectvirtualteacher.models.Course;
 import com.example.finalprojectvirtualteacher.models.User;
 import com.example.finalprojectvirtualteacher.models.dto.UserDtoUpdate;
 import com.example.finalprojectvirtualteacher.services.contacts.CourseService;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -38,38 +40,51 @@ public class UserMvcController {
     public boolean populateIsAuthenticated(HttpSession session) {
         return session.getAttribute("currentUser") != null;
     }
+
     @ModelAttribute("isTeacher")
     public boolean populateIsTeacher(HttpSession session) {
         try {
-            User user=authenticationHelper.tryGetCurrentUser(session);
-            return (user.getRole().getId()==2 || user.getRole().getId()==3);
-        }catch (AuthorizationException e){
+            User user = authenticationHelper.tryGetCurrentUser(session);
+            return (user.getRole().getId() == 2 || user.getRole().getId() == 3);
+        } catch (AuthorizationException e) {
             return false;
         }
     }
 
-    @GetMapping
-    public String showUserPage(HttpSession httpSession, Model model){
+    @ModelAttribute("isAdmin")
+    public boolean populateIsAdmin(HttpSession session) {
         try {
-            User user=authenticationHelper.tryGetCurrentUser(httpSession);
-            if(user.getRole().getId()==1) {
+            User user = authenticationHelper.tryGetCurrentUser(session);
+            return (user.getRole().getId() == 3);
+        } catch (AuthorizationException e) {
+            return false;
+        }
+
+    }
+
+    @GetMapping
+    public String showUserPage(HttpSession httpSession, Model model) {
+        try {
+            User user = authenticationHelper.tryGetCurrentUser(httpSession);
+            if (user.getRole().getId() == 1) {
                 model.addAttribute("loggedIn", user);
                 return "StudentDashboard";
-            }else {
+            } else {
                 model.addAttribute("loggedIn", user);
                 return "InstructorDashboard";
             }
-        }catch (AuthorizationException e){
+        } catch (AuthorizationException e) {
             return "redirect:/auth/login";
         }
     }
-@GetMapping("/profile")
-    public String showProfileInformation(HttpSession httpSession, Model model){
+
+    @GetMapping("/profile")
+    public String showProfileInformation(HttpSession httpSession, Model model) {
         try {
-            User user=authenticationHelper.tryGetCurrentUser(httpSession);
-                model.addAttribute("loggedIn", user);
-                return "ProfileInformation";
-        }catch (AuthorizationException e){
+            User user = authenticationHelper.tryGetCurrentUser(httpSession);
+            model.addAttribute("loggedIn", user);
+            return "ProfileInformation";
+        } catch (AuthorizationException e) {
             return "redirect:/auth/login";
         }
     }
@@ -102,15 +117,15 @@ public class UserMvcController {
     }
 
     @PostMapping("/picture")
-    public String addProfilePhoto(@RequestParam("file") MultipartFile file, HttpSession httpSession){
+    public String addProfilePhoto(@RequestParam("file") MultipartFile file, HttpSession httpSession) {
         try {
-            User user=authenticationHelper.tryGetCurrentUser(httpSession);
-            String url= imageHelper.uploadImage(file);
+            User user = authenticationHelper.tryGetCurrentUser(httpSession);
+            String url = imageHelper.uploadImage(file);
             userService.addProfilePhoto(user, url);
             return "redirect:/users/update";
-        }catch (AuthorizationException e){
+        } catch (AuthorizationException e) {
             return "redirect:/auth/login";
-        } catch (IOException e){
+        } catch (IOException e) {
             return "Error404";
         }
     }
@@ -125,6 +140,41 @@ public class UserMvcController {
             model.addAttribute("completedCourses", courseService.getAllByUserCompleted(user.getId()));
             model.addAttribute("activeCourses", courseService.getAllActiveCoursesNotEnrolled(user));
             return "UserEnrolledCoursesView";
+        } catch (AuthorizationException e) {
+            return "redirect:/auth/login";
+        }
+    }
+
+    @GetMapping("/all")
+    public String showAllUsers(HttpSession session, Model model) {
+        try {
+            User user = authenticationHelper.tryGetCurrentUser(session);
+            List<User> users = userService.getAll();
+            model.addAttribute("users",users);
+            model.addAttribute("loggedIn",user);
+            return "Users";
+        } catch (AuthorizationException e){
+            return "redirect:/auth/login";
+        }
+    }
+    @PostMapping("/{userId}")
+    public String deleteUser(@PathVariable int userId,HttpSession session){
+        try{
+            User user= authenticationHelper.tryGetCurrentUser(session);
+            userService.deleteUser(userId,user);
+        } catch (AuthorizationException e){
+            return "redirect:/auth/login";
+        }
+        return "redirect:/all";
+    }
+    @GetMapping("/allCourses")
+    public String showAllCourses(HttpSession session,Model model) {
+        try{
+            User user = authenticationHelper.tryGetCurrentUser(session);
+            List<Course> courses = courseService.getAll();
+            model.addAttribute("user",user);
+            model.addAttribute("courses",courses);
+            return "AllCourses";
         } catch (AuthorizationException e) {
             return "redirect:/auth/login";
         }
